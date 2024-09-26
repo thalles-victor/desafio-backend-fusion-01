e388aad264b88ee3fa195b37276aec6c68427def
thalles-victor <thalles.victor2012gmail.com>
2024-09-26 15:13:38 -0300

    :wrench: conf: configure typeorm with postgres database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: Number(process.env.POSTGRES_PORT),
+      database: process.env.POSTGRES_ENGLISH_DB,
+      username: process.env.POSTGRES_ENGLISH_USER,
+      password: process.env.POSTGRES_ENGLISH_PASSWORD,
+      url: process.env.POSTGRES_DATABASE_URL,
+      synchronize: process.env.STAGE.toUpperCase() === 'DEV' ? true : false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
