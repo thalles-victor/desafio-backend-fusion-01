46fb08653673263702891bf9f8226c227ef69e15
thalles-victor <thalles.victor2012gmail.com>
2024-09-26 19:49:20 -0300

    :sparkles: feat: implement user typeorm repository and fix ibase reposi.

--- src/Infra/Repositories/IBase.repositories-contract.ts
+++ src/Infra/Repositories/IBase.repositories-contract.ts
@@ -1,6 +1,6 @@
 export interface IBaseRepositoriesContract<Entity, UpdateEntity, UniqueParam> {
   create(entity: Entity): Promise<Entity>;
-  update(updateEntity: UpdateEntity): Promise<Entity>;
+  update(param: UniqueParam, updateEntity: UpdateEntity): Promise<Entity>;
   delete(param: UniqueParam): Promise<void>;
   getBy(param: UniqueParam): Promise<Entity>;
   getMany(): Promise<Entity[]>;
