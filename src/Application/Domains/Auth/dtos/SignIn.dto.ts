a214d4af565b2e8b8227cfe0e6d6a6f61c65e0c4
thalles-victor <thalles.victor2012gmail.com>
2024-09-30 16:13:21 -0300

    :lock: sec: add validators to SignIn and SignUp dtos.

--- src/Application/Domains/Auth/dtos/SignIn.dto.ts
+++ src/Application/Domains/Auth/dtos/SignIn.dto.ts
@@ -1,4 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class SignInDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @Length(1, 255)
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 255)
   password: string;
 }
--- src/Application/Domains/Auth/dtos/SignUp.dto.ts
+++ src/Application/Domains/Auth/dtos/SignUp.dto.ts
@@ -1,5 +1,37 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
+
 export class SignUpDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 255)
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @Length(1, 255)
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 255)
+  @Matches(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/,
+    {
+      message: [
+        'deve conter ao menos um dígito',
+        ' deve conter ao menos uma letra minúscula',
+        ' deve conter ao menos uma letra maiúscula',
+        ' deve conter ao menos um caractere especial',
+        ' deve conter ao menos 8 dos caracteres mencionados',
+      ].toString(),
+    },
+  )
   password: string;
 }
