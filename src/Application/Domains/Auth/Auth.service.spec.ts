f74656e23d6e6fdd4b0e54a2523efc98122995ef
thalles-victor <thalles.victor2012gmail.com>
2024-09-30 17:36:43 -0300

    :test_tube: test: signUp auth service.

--- src/Application/Domains/Auth/Auth.service.spec.ts
+++ src/Application/Domains/Auth/Auth.service.spec.ts
@@ -9,7 +9,7 @@ import { SessionInMemoryRepository } from 'src/Infra/Repositories/Session/Sessio
 import { SignUpDto } from './dtos/SignUp.dto';
 import { UnauthorizedException } from '@nestjs/common';
 
-describe('AuthService', () => {
+describe('AuthService SignIn', () => {
   let authService: AuthService;
 
   beforeEach(async () => {
@@ -80,6 +80,132 @@ describe('AuthService', () => {
           device: 'jest',
           ip_address: '127.0.0.1',
         }),
+    ).rejects.toThrow(UnauthorizedException);
+  });
+});
+
+describe('AuthService SignUp', () => {
+  let authService: AuthService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        JwtModule.register({
+          secret: 'SECRET',
+          global: true,
+        }),
+      ], // Add
+      controllers: [], // Add
+      providers: [
+        {
+          provide: RepoReferenceInjection.USER_REPOSITORY,
+          useClass: UserInMemoryRepository,
+        },
+        {
+          provide: RepoReferenceInjection.SESSION_REPOSITORY,
+          useClass: SessionInMemoryRepository,
+        },
+        AuthService,
+      ], // Add
+    }).compile();
+
+    authService = moduleRef.get<AuthService>(AuthService);
+    // jest.clearAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(authService).toBeDefined();
+  });
+
+  it('It must be possible to log in with an already registered user', async () => {
+    const userDto: SignUpDto = {
+      name: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    const clientInfo = {
+      device: 'jest',
+      ip_address: '127.0.0.1',
+    };
+
+    const result = await authService.signUp(userDto, clientInfo);
+
+    expect(result).toBeDefined();
+
+    const userRegistered = await authService.signIn(
+      {
+        email: userDto.email,
+        password: userDto.password,
+      },
+      clientInfo,
+    );
+
+    expect(userRegistered).toHaveProperty('user');
+    expect(userRegistered).toHaveProperty('user.name', userDto.name);
+    expect(userRegistered).toHaveProperty('user.email', userDto.email);
+    expect(userRegistered).toHaveProperty('access_token');
+    expect(userRegistered).toHaveProperty('session');
+  });
+
+  it('it is not possible to log in with an unregistered user', async () => {
+    const userDto: SignUpDto = {
+      name: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    const clientInfo = {
+      device: 'jest',
+      ip_address: '127.0.0.1',
+    };
+
+    expect(
+      async () =>
+        await authService.signIn(
+          {
+            email: userDto.email,
+            password: userDto.password,
+          },
+          clientInfo,
+        ),
+    ).rejects.toThrow(new UnauthorizedException());
+  });
+
+  it('is not possible to log in with an invalid password or email', async () => {
+    const userDto: SignUpDto = {
+      name: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    const clientInfo = {
+      device: 'jest',
+      ip_address: '127.0.0.1',
+    };
+
+    await authService.signUp(userDto, clientInfo);
+
+    expect(
+      async () =>
+        await authService.signIn(
+          {
+            email: 'wrong@email',
+            password: userDto.password,
+          },
+          clientInfo,
+        ),
+    ).rejects.toThrow(UnauthorizedException);
+
+    expect(
+      async () =>
+        await authService.signIn(
+          {
+            email: userDto.email,
+            password: 'wrong-password',
+          },
+          clientInfo,
+        ),
     ).rejects.toThrow(UnauthorizedException);
   });
 });
